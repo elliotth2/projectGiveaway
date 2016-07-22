05af74808d6aecfae771dc282a96ffca8d9abb55
Hannah Elliott <hannah.elliott@lfpl.org>
2016-07-22 01:40:28 -0400

    adding angular

--- src/app.js
+++ src/app.js
@@ -2,16 +2,8 @@
 var express = require('express');
 
 var app = express();
-
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/templates');
-
 app.use('/', express.static('public'));
 
-
-app.get('/', function(req, res){
-  res.render("index");
-});
 
 app.listen(3000, function() {
   console.log("The frontend server is running on Port 3000.");
