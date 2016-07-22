cd44bdae7fb1b9b9317ae349b8dbb08e5c462c97
Hannah Elliott <hannah.elliott@lfpl.org>
2016-07-22 05:31:27 -0400

    As close as I'm going to make it.

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var entries = require('../models/submissions.js');
-
+var angular= require('vendor/angular.min.js');
 router.post('/submissions', function(req, res){
   var entry= req.body;
-  entries.create(entry, function(){})
-if(err) {
-  return res.status(500).json({err:err.message});
-}
-  res.json({'entry; entry, message: 'data created'});
-  })
+   entries.create(entry, function(){
+      if(err) {
+        return res.status(500).json({err:err.message});
+      }
+      res.json({'entry': entry, 'message': 'data created'});
+    });
+  });
+this. saveEntries = function(entry){
+  var data = [];
+    entry.forEach(function()) {
+        var request;
+      if(!entry._id) {
+        request= $http.post('/models/submission.js')
+      };
+      queue.push(request);
+    });
+    $q.all(queue).then(function(results){
+      console.log("saved to database");
+    });
+  }''
+
 });
